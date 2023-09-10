a8b9d748cbc614d101d396b358d626f3415296f6
Green-Arrow <114510628+Aldair89@users.noreply.github.com>
2023-09-09 20:52:58 -0600

    Add files via upload

---
+++ CalculadoraCientifica.java
@@ -0,0 +1,50 @@
+package com.mycompany.calculadoracientifica;
+import java.util.Scanner;
+public class CalculadoraCientifica {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el primer número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Ingrese el segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("\nElija una operación:");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicación");
+        System.out.println("4. División");
+
+        System.out.print("Ingrese el número de operación: ");
+        int opcion = scanner.nextInt();
+
+        double resultado = 0;
+
+        switch (opcion) {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                } else {
+                    System.out.println("¡Error! No se puede dividir entre cero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("¡Opción inválida!");
+                return;
+        }
+
+        System.out.println("El resultado es: " + resultado);
+    }
+}
